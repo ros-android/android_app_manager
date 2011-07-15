225f10ee5238355aa9e3f03a988c841384e2c3bd
Tony Pratkanis <pratkanis@willowgarage.com>
2011-07-15 16:37:08 -0700

    Formating fix

--- apps/MakeAMap/src/ros/android/makeamap/MakeAMap.java
+++ apps/MakeAMap/src/ros/android/makeamap/MakeAMap.java
@@ -286,7 +286,7 @@ public class MakeAMap extends RosAppActivity implements OnTouchListener {
             public void run() {
               LinearLayout top = (LinearLayout)findViewById(R.id.top_bar);
               LinearLayout.LayoutParams lparams = new LinearLayout.LayoutParams(
-                                                                                LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+                     LinearLayout.LayoutParams.WRAP_CONTENT, LinearLayout.LayoutParams.WRAP_CONTENT);
               Dashboard.DashboardInterface dash = dashboard;
               if (dash != null) {
                 top.addView((View)dash, lparams);

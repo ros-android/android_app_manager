2b7fb9aca06197620f02b8934ff86e7712d90504
Tony Pratkanis <pratkanis@willowgarage.com>
2011-08-15 11:37:12 -0700

    Add script to install all the apps

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cd `rospack find app_chooser`
+ant install
+cd `rospack find pan_tilt`
+ant install
+cd `rospack find make_a_map`
+ant install
+cd `rospack find map_nav`
+ant install
+cd `rospack find android_teleop`
+ant install
+cd `rospack find android_pr2_props`
+ant install
+cd `rospack find voice_recoginition`
+ant install
+
+
